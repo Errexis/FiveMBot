7cac41214ee6353a24f70ec66e4603d74080f8cb
Rafael Velloso <46040272+Rockzer@users.noreply.github.com>
2021-10-04 18:08:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="anakin123",
+    password="",
     database="blastbot"
 )
 mycursor = db.cursor()
